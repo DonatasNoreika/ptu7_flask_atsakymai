700ade783d49684025112a437761b40a2259cbec
Donatas Noreika <donoras@gmail.com>
2023-01-03 08:26:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from calendar import isleap
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/penkis_kartus/<zodis>")
+def penkis_kartus(zodis):
+    return render_template("penkis_kartus.html", zodis=zodis)
+
+@app.route("/visikeliamieji/")
+def visi_keliamieji():
+    masyvas = []
+    for metai in range(1900, 2101):
+        if isleap(metai):
+            masyvas.append(metai)
+    return render_template("visikeliamieji.html", masyvas=masyvas)
+
+if __name__ == "__main__":
+    app.run(debug=True)
